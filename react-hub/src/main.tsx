06c6ad57ad082d22f734460c3757a64c85e70d97
jji-bigG <jerryji@dhcp-vl2041-50651.eduroam.cornell.edu>
2023-08-26 13:05:52 -0400

    mui

--- react-hub/src/main.tsx
+++ react-hub/src/main.tsx
@@ -1,7 +1,11 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const router = createBrowserRouter([
   {
