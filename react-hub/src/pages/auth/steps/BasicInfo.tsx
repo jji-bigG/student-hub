f352d304e655970b0cde9382f53f64fceb457bf1
jerryji <jji.bigg@gmail.com>
2023-12-25 16:40:59 +0800

    validated

--- react-hub/src/pages/auth/steps/BasicInfo.tsx
+++ react-hub/src/pages/auth/steps/BasicInfo.tsx
@@ -3,13 +3,14 @@ import {
   Container,
   CssBaseline,
   Grid,
-  LinearProgress,
   TextField,
   Typography,
 } from "@mui/material";
-import React, { LegacyRef, RefObject } from "react";
+import React, { RefObject } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { userRequests } from "../../../requests";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
 
 interface Props {
   submitRef: RefObject<HTMLButtonElement>;
@@ -17,31 +18,44 @@ interface Props {
   setError: React.Dispatch<React.SetStateAction<string>>;
 }
 
-interface FormProps {
-  firstName: string;
-  lastName: string;
-  username: string;
-  email: string;
-  password: string;
-}
+const schema = z
+  .object({
+    firstName: z.string().min(1, "Required"),
+    lastName: z.string().min(1, "Required"),
+
+    username: z.string().min(1, "Required"),
+    email: z.string().email("Not a valid email format"),
+    password: z.string().min(8, "Password must be at least 8 characters"),
+    confirmPassword: z
+      .string()
+      .min(8, "Password must be at least 8 characters"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "Passwords does not match",
+  });
+
+type FormProps = z.infer<typeof schema>;
 
 export default function BasicInfo({ submitRef, nextStep, setError }: Props) {
   const {
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm<FormProps>();
+    formState: { errors, isValid },
+  } = useForm<FormProps>({ resolver: zodResolver(schema) });
 
   const onSubmit: SubmitHandler<FormProps> = (data) => {
-    userRequests
-      .post("/", data)
-      .then((resp) => {
-        // console.log("Response in register user: ", resp);
-        nextStep();
-      })
-      .catch((e) => {
-        console.log("Error in register user: ", e);
-      });
+    if (isValid)
+      userRequests
+        .post("/", data)
+        .then((resp) => {
+          // console.log("Response in register user: ", resp);
+          nextStep();
+        })
+        .catch((e) => {
+          console.log("Error in register user: ", e);
+          setError("Registration Rejected by Server.");
+        });
   };
 
   return (
@@ -64,9 +78,9 @@ export default function BasicInfo({ submitRef, nextStep, setError }: Props) {
               variant="filled"
               label="First Name"
               required
-              error={errors.firstName?.message === null}
+              error={errors.firstName?.message != null}
               helperText={errors.firstName ? errors.firstName.message : ""}
-              {...register("firstName", { required: true })}
+              {...register("firstName")}
             />
           </Grid>
 
@@ -75,9 +89,9 @@ export default function BasicInfo({ submitRef, nextStep, setError }: Props) {
               variant="filled"
               label="Last Name"
               required
-              error={errors.lastName?.message === null}
+              error={errors.lastName?.message != null}
               helperText={errors.lastName ? errors.lastName.message : ""}
-              {...register("lastName", { required: true })}
+              {...register("lastName")}
             />
           </Grid>
 
@@ -87,13 +101,13 @@ export default function BasicInfo({ submitRef, nextStep, setError }: Props) {
               variant="filled"
               label="Username"
               required
-              error={errors.username?.message === null}
+              error={errors.username?.message != null}
               helperText={
                 errors.username
                   ? errors.username.message
                   : "Username must be unique."
               }
-              {...register("username", { required: true })}
+              {...register("username")}
             />
           </Grid>
 
@@ -103,9 +117,9 @@ export default function BasicInfo({ submitRef, nextStep, setError }: Props) {
               variant="filled"
               label="Email"
               required
-              error={errors.email?.message === null}
-              helperText={errors.email ? errors.email.message : ""}
-              {...register("email", { required: true })}
+              error={errors.email?.message != null}
+              helperText={errors.email?.message}
+              {...register("email")}
             />
           </Grid>
 
@@ -116,9 +130,23 @@ export default function BasicInfo({ submitRef, nextStep, setError }: Props) {
               label="Password"
               type="password"
               required
-              error={errors.password?.message === null}
+              error={errors.password?.message != null}
               helperText={errors.password ? errors.password.message : ""}
-              {...register("password", { required: true })}
+              {...register("password")}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              variant="filled"
+              label="Confirm Password"
+              type="password"
+              required
+              error={errors.confirmPassword?.message != null}
+              helperText={
+                errors.confirmPassword ? errors.confirmPassword.message : ""
+              }
+              {...register("confirmPassword")}
             />
           </Grid>
         </Grid>
